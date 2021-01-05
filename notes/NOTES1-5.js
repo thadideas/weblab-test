41db8d7a6eb735798ddb68d5fc75d08c218f88e6
thad-ideas <yeahthad@gmail.com>
2021-01-05 12:30:50 -0600

    added ideation notes

--- notes/NOTES1-5.js
+++ notes/NOTES1-5.js
@@ -1,1 +1,18 @@
-//yo notes!
+/*yo notes!
+IDEATION AND JS!!
+
+what concepts make a viable product
+diverge, converge, diverge, converge
+1. blue sky --> lots, rough <tag yourself>
+    Problem/Audience/Feature/Theme
+    moodboard
+    don't judge
+    tinder but...
+2. uh smthsmthsmth
+    what is good, what is bad
+3. Transition to viable product
+    focus on BASIC concepts and features
+    work toward simple MVP feat. killer features
+    can we make this more interesting (gameify)
+*/
+
